7e42ebb30eb5f12fc6c7e0d45786ebc02990ba53
mdc-cod <mdcoconta@gmail.com>
2026-04-26 19:13:02 -0300

    matriz de strings na liguagem c

--- C/matrizStrings.c
+++ C/matrizStrings.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 
-char str[]={"string1", "string2", "string3"};
+char *str[]={"string1", "string2", "string3"};
 
 int main()
 {
     
-   printf("s%\ns%\ns%", str[0], str[1], str[2]);
+   printf("%s\n%s\n%s", str[0], str[1], str[2]);
     
     return 0;
 }
