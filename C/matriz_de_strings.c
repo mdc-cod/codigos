52b1e6f00c9ea15dd6f3c83af6cf1b82762d6850
mdc-cod <mdcoconta@gmail.com>
2026-04-26 16:08:19 -0300

    novos comentarios
    Refactor string matrix formation logic to improve clarity and correctness.

--- C/matriz_de_strings.c
+++ C/matriz_de_strings.c
@@ -20,20 +20,25 @@ int main()
     // forma a matriz de strings com cada palavra na string str[]
     for(int i=0; str[i]!='\0'; i++) {
 
+        //pega a primeira palavra
         if(i==0) {
-
+            //  armazena a primeira palavra
             matrizStrings[deslocamento]=&str[i];
             deslocamento+=1;
 
 
         }
-
+        // armazena o restante das palavras, usando o espaço
+        // como referência
+        
         if(str[i]==' ') {
-            i+=1;
-            matrizStrings[deslocamento]=&str[i];
-            deslocamento+=1;
-            i-=1;
-            str[i]=0;
+            
+            i+=1; // se espaço, avança uma posição (para primeira letra da palavra)
+            matrizStrings[deslocamento]=&str[i]; // armazena a string (palavra) apontando ('=&') para a primeira letra dela
+            deslocamento+=1; // para a proxima string (palavra)
+            
+            i-=1; // retorna à posição do espaço para finalizar a string anterior
+            str[i]=0; // finaliza a string (palavra) anterior
         }
     }
 
